33270c07112e9fd81a024f22af8dca0a4709e2e5
Alexandre BM <s@rednaks.tn>
2014-11-16 13:09:09 +0100

    Refcounter + DataContract for serialization

--- test/test/BatteryState.cs
+++ test/test/BatteryState.cs
@@ -1,30 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Phone.Devices.Power;
 
 namespace test
 {
-    class BatteryState
+    [DataContract]
+    public class BatteryState
     {
 
-        BatteryState()
+        public BatteryState(int aRefCount = 0)
         {
-            this.updateState();
+            if(aRefCount != 0) // aRefCount is the last saved ref_count
+                RefCount = aRefCount;
         }
 
+        private static int RefCount = 0;
+
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
         public int level { get; set; }
+        [DataMember]
         public DateTime time { get; set; }
 
-        public void updateState()
+        public BatteryState updateState()
         {
 
             Battery b = Battery.GetDefault();
             this.level = b.RemainingChargePercent;
             this.time = DateTime.Now;
-
+            this.Id = RefCount++;
+            return this;
         }
     }
 }
