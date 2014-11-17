6f4bb3d808a3ae0465097341cce7d2fc08c40444
Alexandre BM <s@rednaks.tn>
2014-11-17 12:07:27 +0100

    ClusteringData Class + timer to schedule ClusteringData update

---
+++ test/MyAgent/ClusteringData.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAgent
+{
+    [DataContract]
+    public class ClusteringData
+    {
+
+        public ClusteringData()
+        {
+            Monday =
+            Tuesday =
+            Thursday =
+            Wednesday =
+            Friday =
+            Saturday =
+            Sunday = 0.0;
+
+            refCount = new List<int>() { 0, 0, 0, 0, 0, 0, 0 };
+        }
+
+        [DataMember]
+        public List<int> refCount; 
+        [DataMember]
+        public double Monday
+        {
+            get { return Monday; }
+            set { 
+                refCount[0]++;
+                Monday += value / refCount[0];
+            }
+        } 
+        [DataMember]
+        public double Tuesday
+        {
+            get { return Tuesday; }
+            set
+            {
+                refCount[1]++;
+                Tuesday += value / refCount[1];
+            }
+        }
+        [DataMember]
+        public double Thursday
+        {
+            get { return Thursday; }
+            set
+            {
+                refCount[2]++;
+                Thursday += value / refCount[2];
+            }
+        }
+        [DataMember]
+        public double Wednesday
+        {
+            get { return Wednesday; }
+            set
+            {
+                refCount[3]++;
+                Wednesday += value / refCount[3];
+            }
+        }
+        [DataMember]
+        public double Friday
+        {
+            get { return Friday; }
+            set
+            {
+                refCount[4]++;
+                Friday += value / refCount[4];
+            }
+        }
+        [DataMember]
+        public double Saturday
+        {
+            get { return Saturday; }
+            set
+            {
+                refCount[5]++;
+                Saturday += value / refCount[5];
+            }
+        }
+        [DataMember]
+        public double Sunday
+        {
+            get { return Sunday; }
+            set
+            {
+                refCount[6]++;
+                Sunday += value / refCount[6];
+            }
+        }
+    }
+}
--- test/MyAgent/ScheduledAgent.cs
+++ test/MyAgent/ScheduledAgent.cs
@@ -2,6 +2,8 @@
 using System.Windows;
 using Microsoft.Phone.Scheduler;
 using System.Collections.Generic;
+using System;
+using System.Threading;
 
 
 namespace MyAgent
@@ -16,6 +18,7 @@ namespace MyAgent
             // The refCount is used to keep track of the last the database id when the app is closed.
             int refCount = IsolatedStorageHelper.GetObject<int>("ref_count");
             new BatteryState(refCount);
+
 
             // Subscribe to the managed exception handler
             Deployment.Current.Dispatcher.BeginInvoke(delegate
@@ -43,15 +46,26 @@ namespace MyAgent
         /// <remarks>
         /// This method is called when a periodic or resource intensive task is invoked
         /// </remarks>
+        /// 
+        private bool firstRun = true;
         protected override void OnInvoke(ScheduledTask task)
         {
             //TODO: Add code to perform your task in background
             LogBatteryState();
+
+            if(firstRun)
+            {
+                SetUpTimer(new TimeSpan(23, 50, 00));
+                firstRun = false;
+            }
             NotifyComplete();
         }
 
+
+       
         private void LogBatteryState()
         {
+            // Storing the battery state.
             List<BatteryState> batteryStateDB = IsolatedStorageHelper.GetObject<List<BatteryState>>("battery_states.db");
 
             if (batteryStateDB == null)
@@ -60,9 +74,68 @@ namespace MyAgent
                 batteryStateDB = new List<BatteryState>();
             }
 
-            batteryStateDB.Add(new BatteryState().updateState());
+            BatteryState lastBatteryState = new BatteryState().updateState();
+
+            batteryStateDB.Add(lastBatteryState);
             IsolatedStorageHelper.SaveObject("battery_states.db", batteryStateDB);
             IsolatedStorageHelper.SaveObject("ref_count", batteryStateDB.Count);
+
+
+
+        }
+
+
+        private void SetUpTimer(TimeSpan alertTime)
+        {
+            DateTime current = DateTime.Now;
+            TimeSpan timeToGo = alertTime - current.TimeOfDay;
+            if (timeToGo < TimeSpan.Zero)
+            {
+                return;//time already passed
+            }
+            System.Threading.Timer timer = new System.Threading.Timer(x =>
+            {
+                this.UpdateClusteringData();
+            }, null, timeToGo, Timeout.InfiniteTimeSpan);
+        }
+
+        private void UpdateClusteringData()
+        {
+            ClusteringData cd = IsolatedStorageHelper.GetObject<ClusteringData>("classification_data.db");
+
+            if (cd == null)
+            {
+                cd = new ClusteringData();
+            }
+
+            switch (DateTime.Today.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    cd.Monday = 0;
+                    break;
+                case DayOfWeek.Tuesday:
+                    cd.Tuesday = 0;
+                    break;
+                case DayOfWeek.Thursday:
+                    cd.Thursday = 0;
+                    break;
+                case DayOfWeek.Wednesday:
+                    cd.Wednesday = 0;
+                    break;
+                case DayOfWeek.Friday:
+                    cd.Friday = 0;
+                    break;
+                case DayOfWeek.Saturday:
+                    cd.Saturday = 0;
+                    break;
+                case DayOfWeek.Sunday:
+                    cd.Sunday = 0;
+                    break;
+            }
+
+
+            Thread.Sleep(120000);
+            SetUpTimer(new TimeSpan(23, 50, 00));
         }
     }
 }
